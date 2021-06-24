5c30fd26d0320e17c24a2cbfe7ca8ef623942149
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-23 21:53:50 -0500

    strict mode added

--- 1 - Conociendo JavaScript/01 - Tu primer JavaScript/js/app.js
+++ 1 - Conociendo JavaScript/01 - Tu primer JavaScript/js/app.js
@@ -1,3 +1,11 @@
-alert('Hola desde un archivo externo');
+'use strict'
+// Strict mode permite garantizar codigo de JS con buenas practicas y no usar palabras reservadas
 
+
+alert('Hola desde un archivo externo');
+// Este es un mensaje a la consola
 console.log('Hola consola')
+
+var x = '3.1416'
+
+var public = 'hola'
