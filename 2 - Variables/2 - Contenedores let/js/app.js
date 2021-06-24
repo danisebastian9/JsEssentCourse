805273bb48960122dd18210145a174c96627e9b8
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-23 22:34:43 -0500

    let variables

--- 2 - Variables/2 - Contenedores let/js/app.js
+++ 2 - Variables/2 - Contenedores let/js/app.js
@@ -1,6 +1,8 @@
 "use strict"
 
-var nombre = 'Sergio';
+// let
+
+var nombre = 'Daniel';
 
 console.log(nombre)
 
@@ -8,10 +10,10 @@ function saludo() {
   let nombre = 'yacafx';
     console.log(nombre)
     
- let edad = 34; 
+  let edad = 34; 
   console.log(edad)
 }
 
- console.log(edad)
+//  console.log(edad) El let es una variable local y no se puede correr fuera de la funcion. Var si es una funcion global
 
 saludo();
