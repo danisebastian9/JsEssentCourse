c8e5460074952f33eb2ed2472b92b8b897966dd4
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-25 17:05:33 -0500

    Logic operators

--- 04 - Operadores/3 - Operadores lógicos/js/app.js
+++ 04 - Operadores/3 - Operadores lógicos/js/app.js
@@ -7,14 +7,14 @@ var datoA = 10;
 var datoB = 20;
 
 // OPERADOR Y o AND - &&
-var and = (datoA > 10 && datoB > 10)
+var and = (datoA > 10 && datoB > 10) // Si ambas se cumplen true 
 console.log('El resultado de la evaluación AND es: ' + and);
 
 // OPERADOR O u OR - ||
-var or = (datoA > 10 || datoB > 10);
+var or = (datoA > 10 || datoB > 10); // Si cualquiera se cumple true
 console.log('El resultado de la evaluación OR es: ' + or);
 
 // OPERADOR DE NEGACIÓN o NOT - !
-var not = !(datoA > 10)
+var not = !(datoA > 10) // Si esta condicion no se cumple
 console.log('El resultado de la evaluación NOT es: ' + not);
 
