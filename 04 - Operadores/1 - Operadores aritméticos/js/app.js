f852485a105d24ecc876efec99ff47ec519a626a
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-25 16:50:21 -0500

    Aritmetic operators

--- 04 - Operadores/1 - Operadores aritméticos/js/app.js
+++ 04 - Operadores/1 - Operadores aritméticos/js/app.js
@@ -1,41 +1,41 @@
 "use strict"
 
-//*** Operadores aritméticos
+//*** Los diferentes Operadores aritméticos
 
 var datoA = 10;
 var datoB = 20;
 
 // SUMA +
-var suma = datoA + datoB;
+var suma = datoA + datoB; // 30
 console.log('La suma de '+ datoA +' + '+ datoB +' es igual a: ', suma);
 
 // RESTA -
-var resta = datoA - datoB;
+var resta = datoA - datoB; // -10
 console.log('La resta de '+ datoA +' - '+ datoB +' es igual a: ', resta);
 
 // MULTIPLICACIÓN *
-var multiplicacion = datoA * datoB;
+var multiplicacion = datoA * datoB; // 200
 console.log('La multiplicación de '+ datoA +' * '+ datoB +' es igual a: ', multiplicacion);
 
 // DIVISIÓN /
-var division = datoA / datoB;
+var division = datoA / datoB; // 0.5
 console.log('La división de '+ datoA +' / '+ datoB +' es igual a: ', division);
 
 // MODULO O RESIDUO %
-var modulo = datoA % datoB;
+var modulo = datoA % datoB; // 10 
 console.log('El módulo o residuo de '+ datoA +' % '+ datoB +' es igual a: ', modulo);
 
 // INCREMENTO ++
-var incremento = datoA;
-incremento++;
+var incremento = datoA; // 11 
+incremento++; // agrega +1 al actual
 
 //incremento = incremento+1
 console.log('El incremento ++ de '+ datoA +' es igual a: ', incremento);
 
 
 // DECREMENTO --
-var decremento = datoA;
-decremento--;
+var decremento = datoA; //9
+decremento--; // resta -1 al actual
 
 //decremento = decremento -1
 console.log('El decremento -- de '+ datoA +' es igual a: ', decremento);
