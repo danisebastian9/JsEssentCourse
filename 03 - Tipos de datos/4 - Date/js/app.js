7184907ef62d6c297c0eb6622355d9e6c03143e5
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-24 21:52:05 -0500

    Date type

--- 03 - Tipos de datos/4 - Date/js/app.js
+++ 03 - Tipos de datos/4 - Date/js/app.js
@@ -4,4 +4,9 @@
 
 var fecha = new Date();  // Esta variable tiene la fecha exacta completa del momento en que se ejecuta
 
+fecha.getDay() // El dia de la semana en que nos encontramos. Martes(2)
+fecha.getDate() // El dia del mes en que nos encontramos Jun 24 (24)
+fecha.getHours() // Cual es la hora en que se ejecuta la instruccion
+fecha.setDate(5) // devuelve un valor numero convertido en fecha. En este caso cambia la fecha al dia 5 del mes y año actual. 
+
 
