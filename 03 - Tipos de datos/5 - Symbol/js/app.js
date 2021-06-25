7f5504d952586f61ad88413c420d9680f5391597
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-24 21:56:08 -0500

    Symbols

--- 03 - Tipos de datos/5 - Symbol/js/app.js
+++ 03 - Tipos de datos/5 - Symbol/js/app.js
@@ -1,6 +1,10 @@
 "use strict"
 
+// SYMBOLS son unicos e inmutables. Dato unico alrededor el programa
+
 var simbolo1 = Symbol();
 var simbolo2 = Symbol();
+
+simbolo1 === simbolo2 // false. 
 
 var ambiente = Symbol('dev');
