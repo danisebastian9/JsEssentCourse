e70ad0e563a0ea1d7b054cce2509aeb3b5e6c098
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-24 21:43:45 -0500

    String type

--- 03 - Tipos de datos/2 - Texto/js/app.js
+++ 03 - Tipos de datos/2 - Texto/js/app.js
@@ -1,11 +1,12 @@
 "use strict"
 
-var bebida = "agua";
+var bebida = "agua"; // Comillas dobles igual a comillas sencillas 
 
 var comida = 'ceviche';
 
-var instrucción = "El platillo se llama 'ceviche' ";
+var instrucción = "El platillo se llama 'ceviche' "; // Combinacion de comillas sencillas con dobles
 
-var edad = 34;
+var edad = 34;  // numero entero    
 
-var edadtxt = String(edad)
+var edadtxt = String(edad) // convertido a string
+
