d8fdf5a48ad9e0392ca7e840fd60efba27b8f634
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-24 21:40:51 -0500

    Number type

--- 03 - Tipos de datos/1 -  Numéricos/js/app.js
+++ 03 - Tipos de datos/1 -  Numéricos/js/app.js
@@ -1,11 +1,11 @@
 "use strict"
 
-var edad = 35;
+var edad = 35; // 34 numero natural, 34.00 numero decimal, -34 numero negativo
 
-var cantidad = "100";
+var cantidad = "100"; // string. 
 
-var nuevaCantidad = Number(cantidad);
+var nuevaCantidad = Number(cantidad); // convertir string a numero
 
-parseInt()
-
-parseFloat()
+parseInt()  // Convertir datos a enteros
+ 
+parseFloat() // Convertir datos a flotantes
