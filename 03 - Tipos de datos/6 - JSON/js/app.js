f1725fa55f0f6a748e796d73cefde3e0d63f916e
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-24 22:01:25 -0500

    Objects type

--- 03 - Tipos de datos/6 - JSON/js/app.js
+++ 03 - Tipos de datos/6 - JSON/js/app.js
@@ -2,15 +2,25 @@
 
 // JSON => JavaScript Object Notation
 
-var persona = {nombre: 'Sergio', twitter: 'yacafx'};
+var persona = {nombre: 'Daniel', twitter: 'myTwitter'};
+
+persona.nombre // 'Daniel'
 
 var personas = [
     {nombre: 'Hugo', twitter: 'dcHugo'},
     {nombre: 'Paco', twitter: 'dcPaco'},
     {nombre: 'Luis', twitter: 'dcLuis'},
-    persona
+    persona // agrega el primer objeto a este.
 ]
 
-var personaJSON = JSON.stringify(persona);
+personas // todo el objeto 
+
+
+var personaJSON = JSON.stringify(persona); 
+// Convierte todo en una cadena de texto para enviarla a un servidor o DB
+
 
 var nuevaPersona = JSON.parse(personaJSON)
+// Reestructurar a JSON. Convierte la cadena de texto en formato JSON
+
+
