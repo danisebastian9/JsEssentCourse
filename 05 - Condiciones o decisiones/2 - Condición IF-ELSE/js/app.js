6edb90c6760939702955dac0af72eab9228c14ad
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-06-30 21:29:44 -0500

    IF - ELSE

--- 05 - Condiciones o decisiones/2 - Condición IF-ELSE/js/app.js
+++ 05 - Condiciones o decisiones/2 - Condición IF-ELSE/js/app.js
@@ -8,9 +8,9 @@ var datoB = 20;
 var resultado = "Sin datos";
 
 if (datoA > datoB){
-	resultado = "La condición se cumplió";
+	resultado = "La condición se cumplió"; // Si la condicion se cumple
 } else {
-    resultado = "La condición no se cumplió";
+    resultado = "La condición no se cumplió"; // Si no entonces
 }
 
 console.log("El resultado de la evaluación if-else es: ", resultado);
